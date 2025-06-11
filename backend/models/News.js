354494afe2ea18c896466d6544722cf834ab8ee8
Dawud <38299773+dagtechsolutions@users.noreply.github.com>
2025-06-11 19:48:59 +0100

    Update News.js

--- backend/models/News.js
+++ backend/models/News.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const NewsSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    imageUrl: { type: String },
-    author: { type: String },
-    date: { type: Date, default: Date.now }
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  category: { type: String }, // added category
+  imageUrl: { type: String },
+  author: { type: String },
+  date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('News', NewsSchema);
